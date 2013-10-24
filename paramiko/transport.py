2c538bd5fe7c72ce8cf5e67ee86b28770f62c11b
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2013-10-24 19:59:57 -0400

    [Python 3]: Hack around __slots__ and properties conflict.

--- paramiko/transport.py
+++ paramiko/transport.py
@@ -83,7 +83,8 @@ class SecurityOptions (object):
     C{ValueError} will be raised.  If you try to assign something besides a
     tuple to one of the fields, C{TypeError} will be raised.
     """
-    __slots__ = [ 'ciphers', 'digests', 'key_types', 'kex', 'compression', '_transport' ]
+    # TODO: Come up a more elegant fix...
+    #__slots__ = [ 'ciphers', 'digests', 'key_types', 'kex', 'compression', '_transport' ]
 
     def __init__(self, transport):
         self._transport = transport
